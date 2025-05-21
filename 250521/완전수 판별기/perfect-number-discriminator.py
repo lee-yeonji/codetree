251ff7265cf63b3546f52f789c667c4feae8a837
leeyeonji <101976270+lee-yeonji@users.noreply.github.com>
2025-05-21 18:01:47 +0900

    Title: 완전수 판별기, Time: 39ms, Memory: 16MB, Status: Passed - Codetree

--- 250521/완전수 판별기/perfect-number-discriminator.py
+++ 250521/완전수 판별기/perfect-number-discriminator.py
@@ -1,11 +1,13 @@
 n = int(input())
 sum_val = 0
 
-for i in range(1, n+1):
+# 1부터 n-1까지의 수 중에서 약수를 찾는다.
+for i in range(1, n):
     if n % i == 0:
         sum_val += i
 
-if sum_val % i == 0:
+# sum_val과 n이 같으면 P, 다르면 N을 출력한다.
+if sum_val == n:
     print('P')
 else:
     print('N')
