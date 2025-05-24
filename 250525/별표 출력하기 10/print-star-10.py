d1b6e30c6da8f5e6a0f91705e59679b5fe64c047
leeyeonji <101976270+lee-yeonji@users.noreply.github.com>
2025-05-25 02:09:18 +0900

    Title: 별표 출력하기 10, Time: 39ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250525/별표 출력하기 10/print-star-10.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(2 * n):
+    # 짝수 줄 출력
+    if i % 2 == 0:
+        for _ in range((i//2)+1):
+            print('*', end=' ')
+    
+    # 홀수 줄 출력
+    else:
+        for _ in range(n-(i//2), 0, -1):
+            print('*', end=' ')
+
+    print()
