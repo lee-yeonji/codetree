d0423ca0e3430771d076c2711e415bd1c9122e91
leeyeonji <101976270+lee-yeonji@users.noreply.github.com>
2025-05-24 21:12:11 +0900

    Title: 별표 출력하기 6, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250524/별표 출력하기 6/print-star-6.py
+++ 250524/별표 출력하기 6/print-star-6.py
@@ -1,5 +1,7 @@
 n = int(input())
 
+'''문제 풀이
+# 모양에 맞게 윗쪽 별 출력 (역삼각형)
 for i in range(n):
     for j in range(i):
         print(' ', end=' ')
@@ -7,9 +9,25 @@ for i in range(n):
         print('*', end=' ')
     print()
 
+# 모양에 맞게 아랫쪽 별 출력 (정삼각형)
 for i in range(n-1):
     for j in range(n-i-2):
         print(' ', end=' ')
     for j in range(2 * (i + 1) + 1):
+        print('*', end=' ')
+    print()
+'''
+#모범 답안
+for i in range(n):
+    for _ in range(i):
+        print(' ', end=' ')
+    for _ in range((2 * n)-(2 * i)-1):
+        print('*', end=' ')
+    print()
+
+for i in range(n-2, -1, -1):
+    for _ in range(i):
+        print(' ', end=' ')
+    for _ in range((2 * n) - (2 * i) - 1):
         print('*', end=' ')
     print()
