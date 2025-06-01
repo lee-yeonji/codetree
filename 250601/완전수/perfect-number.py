a84080c267293f979d1581514fe10f73f657ab33
leeyeonji <101976270+lee-yeonji@users.noreply.github.com>
2025-06-01 16:41:50 +0900

    Title: 완전수, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 250601/완전수/perfect-number.py
+++ 250601/완전수/perfect-number.py
@@ -1,9 +1,10 @@
 start, end = map(int, input().split())
 cnt = 0
 
-# Please write your code here.
 for i in range(start, end+1):
+    # n의 진약수의 총 합 = sum_val
     sum_val = 0
+    
     for j in range(1, i+1):
         if i % j == 0 and i != j:
             sum_val += j
