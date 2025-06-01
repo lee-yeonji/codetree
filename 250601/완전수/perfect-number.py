ad858f5c27813cbac85bfd3dbe2f9a5fe34d1f7a
leeyeonji <101976270+lee-yeonji@users.noreply.github.com>
2025-06-01 16:22:51 +0900

    Title: 완전수, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250601/완전수/perfect-number.py
@@ -0,0 +1,12 @@
+start, end = map(int, input().split())
+cnt = 0
+sum_val = 0
+
+# Please write your code here.
+for i in range(start, end+1):
+    for j in range(1, i+1):
+        if i % j == 0 and i != j:
+            sum_val += j
+    if sum_val == i:
+        cnt += 1
+print(cnt)
