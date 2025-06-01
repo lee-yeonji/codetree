f9bf77eccf030ff417be9c1a01b9563710a4ca87
leeyeonji <101976270+lee-yeonji@users.noreply.github.com>
2025-06-02 00:06:07 +0900

    Title: 배열 채우기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250602/배열 채우기/filling-array.py
+++ 250602/배열 채우기/filling-array.py
@@ -1,7 +1,12 @@
 arr = list(map(int, input().split()))
+new_arr = []
 
-# 배열을 뒤집어서 출력
-for elem in arr[::-1]:
+# 배열에 0이 있는 경우, 0 제거
+for elem in arr:
     if elem == 0:
         continue
-    print(elem, end=' ')
+    new_arr.append(elem)
+
+# 배열을 뒤집어서 출력
+for i in new_arr[::-1]:
+    print(i, end=' ')
