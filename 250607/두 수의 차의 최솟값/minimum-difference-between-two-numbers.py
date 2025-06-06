0c52df10b946a178a2aa94f5e692641ff8efdf0d
leeyeonji <101976270+lee-yeonji@users.noreply.github.com>
2025-06-07 01:00:38 +0900

    Title: 두 수의 차의 최솟값, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250607/두 수의 차의 최솟값/minimum-difference-between-two-numbers.py
+++ 250607/두 수의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -12,3 +12,22 @@ for i in range(n):
             sub.append(nums[j] - nums[i])
 
 print(min(sub))
+
+'''모범 답안
+# 변수 선언 및 입력
+n = int(input())
+arr = list(map(int, input().split()))
+
+# 숫자들이 오름차순으로 주어지기 때문에,
+# 두 숫자의 차의 최솟값은 반드시 인접한 두 숫자의 차 중에 최솟값이 있습니다.
+# 초기값을 적습니다. 최소가 될 첫 번째 후보입니다.
+min_val = arr[1] - arr[0]
+
+# 나머지 원소들을 보며 최솟값을 갱신합니다.
+for i in range(2, n):
+	if min_val > arr[i] - arr[i - 1]:# 지금까지 나왔던 값들 보다 더 작은 값이라면
+		min_val = arr[i] - arr[i - 1]  # 최솟값이 되므로 그 값을 갱신합니다.
+
+# 출력
+print(min_val)
+'''
