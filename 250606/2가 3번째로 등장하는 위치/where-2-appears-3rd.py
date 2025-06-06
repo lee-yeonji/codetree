8fd1a1c10cf84a004efa4de3f5382d241d12e548
leeyeonji <101976270+lee-yeonji@users.noreply.github.com>
2025-06-06 23:10:12 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250606/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 250606/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -1,11 +1,10 @@
 n = int(input())
 arr = list(map(int, input().split()))
-cnt, result = 0, 0
+cnt = 0
 
 # 리스트 내 원소와 index가 동시에 필요한 경우 => enumerate() 함수 이용
 for i, elem in enumerate(arr):
     if elem == 2:
         cnt += 1
         if cnt == 3:
-            result = i
-print(result+1)
+            print(i + 1)
