90e0febeb15ae9321eea7cb478e4b9c78a9c1065
leeyeonji <101976270+lee-yeonji@users.noreply.github.com>
2025-06-06 22:54:04 +0900

    Title: 연속부분수열일까, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250606/연속부분수열일까/contiguous-array-or-not.py
+++ 250606/연속부분수열일까/contiguous-array-or-not.py
@@ -3,10 +3,11 @@ a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 answer = False
 
-for i in range(n1):
-    for j in range(n2):
-        if b[j] == a[i] and b[j-1] == a[i-1]:
-            answer = True
+# 수열 B가 수열 A의 연속부분수열인지 확인 : ex) A = [1,2,3,4], B = [1,2] => True
+for i in range(n1-n2+1):
+    # 수열 B가 수열 A의 부분과 순서까지 완전 일치하는 지 확인
+    if a[i:i+len(b)] == b:
+        answer = True
 
 # 수열 B가 수열 A의 연속부분수열인지 출력
 if answer == True:
