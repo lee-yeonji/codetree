133ef6e5ef11a1d1630357a667d5042f564bf6ab
leeyeonji <101976270+lee-yeonji@users.noreply.github.com>
2025-06-06 23:18:02 +0900

    Title: 가장 큰 수 고르기, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250606/가장 큰 수 고르기/picking-biggest-number.py
+++ 250606/가장 큰 수 고르기/picking-biggest-number.py
@@ -1,8 +1,8 @@
-import sys
+import sys # 모듈을 불러오는 역할: sys라는 모듈을 불러오기
 
 arr = list(map(int, input().split()))
 
-max_val = -sys.maxsize
+max_val = -sys.maxsize # 파이썬 안에서 최소 정수값 설정 (sys.maxsize: 최대 정수값 설정)
 for elem in arr:
     if elem > max_val:
         max_val = elem
